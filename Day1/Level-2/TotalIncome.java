fb0f269ec7509eb46fabaf4a0eb22c54314d62da
srijan0412 <srijan.soni01@gmail.com>
2025-01-09 20:00:56 +0530

    Total Income

---
+++ Day1/Level-2/TotalIncome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class TotalIncome {
+    public static void main(String[] args) {
+
+        //Initializing the Scanner object
+        Scanner scanner = new Scanner(System.in);
+        
+        //Taking input from the user
+        System.out.print("Enter salary: ");
+        double salary = scanner.nextDouble();
+        
+        System.out.print("Enter bonus: ");
+        double bonus = scanner.nextDouble();
+        
+        // Calculation of Total income
+        double totalIncome = salary + bonus;
+
+        System.out.printf("The salary is INR " + salary + " and bonus is INR " + bonus + ". Hence Total Income is INR " + totalIncome);
+        scanner.close();
+    }
+}
