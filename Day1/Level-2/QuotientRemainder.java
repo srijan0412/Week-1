25261624ae4757e6fec9a605f14760d3802815d1
srijan0412 <srijan.soni01@gmail.com>
2025-01-09 19:16:36 +0530

    Quotient Remainder

---
+++ Day1/Level-2/QuotientRemainder.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+
+        //Initializing the Scanner object
+        Scanner scanner = new Scanner(System.in);
+        
+        // Taking number1 & number2 as input from the user
+        System.out.print("Enter first number: ");
+        int number1 = scanner.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int number2 = scanner.nextInt();
+        
+        // Calculation of Quotient and remainder
+        int quotient = number1 / number2;
+        int remainder = number1 % number2;
+        
+        System.out.println("The Quotient is " + quotient + " and Remainder is " + remainder + " of two numbers " + number1 + " and " + number2);
+        
+        scanner.close();
+    }
+}
