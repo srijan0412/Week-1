9c69aca592d58575853cf96908e0b34beb8d1e3b
srijan0412 <srijan.soni01@gmail.com>
2025-01-10 22:18:50 +0530

    Multiplication Table

---
+++ Day3/Level-1/MultiplicationTable.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class MultiplicationTable{
+	public static void main(String [] args){
+		//Initializing a Scanner class object 
+		Scanner sc = new Scanner(System.in);
+		
+		//Defining an array of size 10
+		int arr [] = new int[10];
+		
+		//Taking input from the user
+		System.out.print("Enter the number: ");
+		int number = sc.nextInt();
+		
+		//Storing multiplication of number in array
+		for(int i=0; i<10; i++){
+			arr[i] = number * (i+1);
+			System.out .println(number+" * "+ (i+1) + " = "+ arr[i]);
+		}
+
+        // closing the scanner object 
+        sc.close();
+	}
+}
