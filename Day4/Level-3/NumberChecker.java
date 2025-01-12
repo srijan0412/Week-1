6efe3b3a743d3012534d2e53e4fbe107ec066080
srijan0412 <srijan.soni01@gmail.com>
2025-01-12 20:27:17 +0530

    Further extending the Number Checker class

--- Day4/Level-3/NumberChecker.java
+++ Day4/Level-3/NumberChecker.java
@@ -169,6 +169,56 @@ class NumberChecker {
         return frequencyArray;
     }
 
+    
+    // Method to check if a number is prime
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Method to check if a number is a neon number
+    public static boolean isNeon(int number) {
+        int square = number * number;
+        int sumOfDigits = sumOfDigits(square);
+
+        return sumOfDigits == number;
+    }
+
+    // Method to check if a number is a spy number
+    public static boolean isSpy(int number) {
+        int sum = sumOfDigits(number);
+        int product = 1;
+        
+        while (number > 0) {
+            int digit = number % 10;
+            product *= digit;
+            number /= 10;
+        }
+
+        return sum == product;
+    }
+
+    // Method to check if a number is an automorphic number
+    public static boolean isAutomorphic(int number) {
+        int square = number * number;
+        String squareStr = Integer.toString(square);
+        String numberStr = Integer.toString(number);
+
+        return squareStr.endsWith(numberStr);
+    }
+
+    // Method to check if a number is a buzz number
+    public static boolean isBuzz(int number) {
+        return number % 7 == 0 || number % 10 == 7;
+    }
+
 
     public static void main(String[] args) {
         int number = 2457;
@@ -208,5 +258,10 @@ class NumberChecker {
                 System.out.println(frequency[i][0] + " : " + frequency[i][1]);
             }
         }
+        System.out.println("Is it a prime number? " + isPrime(number));
+        System.out.println("Is it a neon number? " + isNeon(number));
+        System.out.println("Is it a spy number? " + isSpy(number));
+        System.out.println("Is it an automorphic number? " + isAutomorphic(number));
+        System.out.println("Is it a buzz number? " + isBuzz(number));
     }
 }
